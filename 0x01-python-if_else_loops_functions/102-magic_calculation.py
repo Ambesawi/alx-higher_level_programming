c662b13c0c0259cf879c873b1ad1d9fea5cf2ebe
Ambesawi <ambesawi@gmail.com>
2023-03-07 13:53:10 -0800

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+102-magic_calculation.py
+
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
