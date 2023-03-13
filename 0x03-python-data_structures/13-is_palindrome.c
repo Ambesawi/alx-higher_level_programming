b60a63c7e196b9b2f37cac067f5e144918870d37
Ambesawi <ambesawi@gmail.com>
2023-03-13 13:01:56 -0700

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *is_palindrome - ispalindrome
+ *@head: list
+ *Return: number elements.
+ */
+int is_palindrome(listint_t **head)
+{
+	return (0);
+}
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,4 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+int is_palindrome(listint_t **head);
+#endif
