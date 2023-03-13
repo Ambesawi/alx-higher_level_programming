10a0ab3aa6525762fcd13ad07ecee22bc07b5979
Ambesawi <ambesawi@gmail.com>
2023-03-13 13:03:07 -0700

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+/**
+ *print_python_list_info - hola
+ *@p: list
+ *Return: number elements.
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
