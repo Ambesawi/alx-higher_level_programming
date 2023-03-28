a7cf650d2518808f66d90edbc3283171e03d3c58
Ambesawi <ambesawi@gmail.com>
2023-03-28 11:20:37 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,6 +7,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
