5d9884129760455bb184d8a6998fd83dd7155ee3
Ambesawi <ambesawi@gmail.com>
2023-04-08 14:48:37 -0700

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def copy_list(l):
-    """
-
-    Returns a copy of a given list.
-
-    :param l: the list to be copied
-    :return: a copy of the input list
-    """
-
+    """  :param l: the list to be copied  """
     return l.copy()
