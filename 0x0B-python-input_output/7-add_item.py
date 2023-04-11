b3616071643ac4cd1d038cd087f0ff4cb347a0cd
Ambesawi <ambesawi@gmail.com>
2023-04-11 14:19:49 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Add all arguments to Python list and save them to a file on this project."""
+"""Add all arguments to a Python list and save them to a file."""
+
 import sys
 
-
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+        __import__('8-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
