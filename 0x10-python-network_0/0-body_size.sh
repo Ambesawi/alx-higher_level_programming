85aa763d59b453d4e397df94087db64e45a31870
Ambesawi <ambesawi77@gmail.com>
2023-06-30 14:20:01 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# takes in a URL, sends a request to that URL, and display
 
 curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
