c4ab6a38b79e3ef1499e08401bd8146cad9c465d
Ambesawi <ambesawi77@gmail.com>
2023-06-30 14:29:27 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL
-# and display only body of a 200 status code response
+# takes in a URL, sends a GET request to the URL, and display only body of a 200 status code response
 curl -s "$1" -X GET -L
