84c0beee6c139531e49724de22ff844d7cbfd2d7
Ambesawi <ambesawi77@gmail.com>
2023-07-25 17:22:04 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+    for (let k = 0; k < body.length; ++k) {
+      const userId = body[k].userId;
+      const completed = body[k].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
       }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
-  });
-  console.log(tasksCompleted);
+
+    console.log(completedTasksByUsers);
+  }
 });
